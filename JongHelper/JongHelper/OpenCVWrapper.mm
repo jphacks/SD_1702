62b0d32c975a85d8000f9f30776585fc4d8d8561
kobasato34 <weakest34@gmail.com>
2017-10-21 10:43:15 +0900

    OpenCVの準備したりAVCaptureあたりを準備したり

---
+++ JongHelper/JongHelper/OpenCVWrapper.mm
@@ -0,0 +1,16 @@
+//
+//  OpenCVWrapper.m
+//  JongHelper
+//
+//  Created by Satoshi Kobayashi on 2017/10/21.
+//  Copyright © 2017年 tomato. All rights reserved.
+//
+
+#import <opencv2/opencv.hpp>
+#import <opencv2/imgcodecs/ios.h>
+
+#import "OpenCVWrapper.h"
+
+@implementation OpenCVWrapper
+
+@end
--- JongHelper/JongHelper/ViewController.swift
+++ JongHelper/JongHelper/ViewController.swift
@@ -8,11 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureDelegate, UIGestureRecognizerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
+    private let avCapture = AVCapture()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        avCapture.delegate = self
+        
+        //タップジェスチャー
+        let tapGesture:UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(ViewController.tapped(_:)))
+        // デリゲートをセット
+        tapGesture.delegate = self
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func tapped(_ sender: UITapGestureRecognizer){
+        if sender.state == .ended {
+            //画面タップ時
+            //setTehaiView(tehaiArray, animated: true)
+            avCapture.takePicture()
+        }
+    }
+    
+    func capture(image: UIImage) {
+        imageView.image = image
+    }
+    
+    func photo(image: UIImage) {
+        
     }
 
     override func didReceiveMemoryWarning() {
