94d13dd004b4ba59cff215bcf47444daa5ec28cd
kobasato34 <weakest34@gmail.com>
2017-11-08 14:49:55 +0900

    ノーテン時のビューが強くなった（とりあえず狙えそうな役を表示するようにした）

---
+++ JongHelper/JongHelper/NotenYakuView.swift
@@ -0,0 +1,20 @@
+//
+//  NotenYakuView.swift
+//  JongHelper
+//
+//  Created by Satoshi Kobayashi on 2017/11/08.
+//  Copyright © 2017年 tomato. All rights reserved.
+//
+
+import UIKit
+
+class NotenYakuView : UIView {
+    //@IBOutlet var haiImage: [UIImageView]!
+    @IBOutlet weak var syantenLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+}
